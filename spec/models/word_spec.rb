62a13c46cf3a8d53dc71ecdc688fad79093e6fb6
Leandro Trombini <leandro.trombini@locaweb.com.br>
2023-04-26 15:14:02 -0300

    Adds validation to the model word

--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -1,6 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Word, type: :model do
-  it { is_expected.to have_db_column(:value) }
-  it { is_expected.to have_db_column(:language) }
+  describe 'columns' do
+    it { is_expected.to have_db_column(:value) }
+    it { is_expected.to have_db_column(:language) }
+  end
+
+  describe 'vadations' do
+    it { is_expected.to validate_presence_of(:value) }
+    it { is_expected.to validate_presence_of(:language) }
+
+    subject { described_class.new }
+
+    it "is valid with valid attributes 1" do
+      subject.value = "Anything"
+      subject.language = "Anything"
+
+      expect(subject).to be_valid
+    end
+
+    it "is valid with valid attributes 2" do
+      word = Word.new(value: 'teste', language: 'PT')
+      expect(word).to be_valid
+    end
+
+    it "is not valid without a language" do
+      word = Word.new(value: 'teste', language: nil)
+      expect(word).to_not be_valid
+    end
+  end
 end
