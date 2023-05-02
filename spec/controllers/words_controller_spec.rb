e383f22b1e06eefe3d272f19cbd069fb46217287
Leandro Trombini <leandro.trombini@locaweb.com.br>
2023-05-02 10:24:45 -0300

    Adds more tests

--- spec/controllers/words_controller_spec.rb
+++ spec/controllers/words_controller_spec.rb
@@ -7,4 +7,24 @@ RSpec.describe WordsController, type: :controller do
       expect(response).to be_successful
     end
   end
+
+  describe "POST create" do
+    subject { post :create, params: params }
+
+    context 'valid params' do
+      let(:params) { { word: { value: 'cat', language: 'english'} } }
+
+      it "creates new word" do
+        expect { subject }.to change { Word.count }.by 1
+      end
+    end
+
+    context 'invalid params' do
+      let(:params) { { word: { value: 'cat' } } }
+
+      it "does not create new word" do
+        expect { subject }.to_not change(Word, :count)
+      end
+    end
+  end
 end
--- spec/requests/words_request_spec.rb
+++ spec/requests/words_request_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 RSpec.describe "Words", type: :request do
   describe "GET /words" do
-     it "returns http success" do
-         word = Word.create(value: "teste", language: "Português")
-        get "/words"
-        expect(response.status).to eq(200)
-        expect(response.body).to include("teste")
-     end
+    it "returns http success" do
+      word = Word.create(value: "teste", language: "Português")
+      get "/words"
+      expect(response.status).to eq(200)
+      expect(response.body).to include("teste")
+    end
   end
 end
